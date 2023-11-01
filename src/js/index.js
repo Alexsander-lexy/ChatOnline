dac3e9db321522da1bdb19c996a060b5587aa585
Adryel Santos <adryeljuniods@gmail.com>
2023-11-01 19:24:25 -0300

    Up code

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,17 @@ const formulario = document.getElementById('formulario');
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
 
+    const input = document.getElementById("mensagem");
+    const pFrase = document.createElement('p');
 
-    
-})
+    if(input.value){
+        pFrase.classList.add('pFrase');
+        const mensagemEnviada = document.getElementById('mensagem-enviada');
+        pFrase.innerHTML = `<p class="MSG1">${input.value}</p>`;
+        mensagemEnviada.appendChild(pFrase);
+        input.value.innerHTML = '';
+    }else{  
+        return;
+    }
+   
+});
